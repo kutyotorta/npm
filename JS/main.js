beeadd0a471445f4c6c5b146c7a340027515560c
Erica <kutyotorta@gmail.com>
2020-07-29 22:02:05 +0200

    init

--- JS/main.js
+++ JS/main.js
@@ -10,24 +10,39 @@ function getServerData(url) {
         response=>response.json(),
         err=>console.error(err)
     )
-    };
+};
 
-   document.querySelector("#getDataBtn").addEventListener("click", function(){
+document.querySelector("#getDataBtn").addEventListener("click", function(){
     getServerData("http://localhost:3000/users").then(
         data => fillDataTable(data, "UserTable")
     );  
-   })
+})
   
-   //Fill table with Server data.
+//Fill table with Server data. (Táblázat feltöltése a szerverről származó adattal)
+function fillDataTable(data, tableID){
+    let table= document.querySelector(`#${tableID}`);
+    
+    if (!table){
+        console.error(`Table "${tableID}" is not found.`);
+        return;
+    }
 
-   function fillDataTable(data, tableID){
-       let table= document.querySelector('#${tableID}');
-       if (!table){
-           console.error('Table "${tableID}" is nout found.');
-           return;
-       }
-       let tBody = table.querySelector("tbody");
-       for (let row of data){
-           console.log(row);
-       }
-   }
+    let tBody = table.querySelector("tbody");
+    for (let row of data){
+        let tr= createAnyElement("tr");
+        for (let k in row){
+            let td=createAnyElement("td");
+            td.innerHTML= row[k];
+            tr.appendChild(td);
+        }
+        tBody.appendChild(tr);   
+    }
+}
+
+function createAnyElement(name, attributes){
+    let element= document.createElement(name);
+    for (let k in attributes){
+        element.setAttribute(k, attributes[k]);
+    }
+    return element;
+}
--- npm/README.md
+++
@@ -1,2 +0,0 @@
-# npm
-Admin felület
